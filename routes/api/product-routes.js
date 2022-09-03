27d59a1a5d3f48a8bb22b0b0b7c2221dfe18bae6
ccaitano <cheryl.caitano@gmail.com>
2022-09-03 11:29:50 -1000

    Updated put and delete routes for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -31,19 +31,9 @@ router.get('/:id', async (req, res) => {
       return;
     }
 
-    res.status(400).json(productData);
+    res.status(200).json(productData);
   } catch {
     res.status(500).json(err);
-  }
-});
-
-router.post('/', async (req, res) => {
-  // create a new category
-  try {
-    const categoryData = await Category.create(req.body);
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(400).json(err);
   }
 });
 
